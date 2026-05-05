8c0faab74abde5e7264c21bcbcf6ad951ec3e943
likithaalahari1 <likithaalahari03@gmail.com>
2026-05-05 15:27:48 +0530

    Update urls.py

--- backend/eventflow/urls.py
+++ backend/eventflow/urls.py
@@ -1,5 +1,14 @@
+from django.http import JsonResponse
 from django.urls import include, path
 
+
+def health_check(request):
+    return JsonResponse({
+        "status": "healthy"
+    })
+
+
 urlpatterns = [
+    path("", health_check),
     path("api/", include("api.urls")),
 ]
